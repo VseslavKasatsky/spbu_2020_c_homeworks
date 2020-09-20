e11906e099649be6fbb3057ed0c0114a2a786a2c
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-20 14:40:34 +0300

    Style Guide FIX

--- Homework_2/2.task_3.c
+++ Homework_2/2.task_3.c
@@ -2,13 +2,13 @@
 const int N = 11;
 int main()
 {
-	int arr[11] = { 5,0,1,5,2,0,6,0,3,7,0 };
+	int arr[11] = { 5, 0, 1, 5, 2, 0, 6, 0, 3, 7, 0 };
 	int i = 0;
 	for (i = 0; i < N; i++)
 	{
 		if (arr[i] == 0)
 		{
-			for (int j = i; j < N-1; j++)
+			for (int j = i; j < N - 1; j++)
 			{
 				arr[j] = arr[j + 1];
 			}
