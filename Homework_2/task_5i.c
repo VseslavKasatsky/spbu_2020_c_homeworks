f9f8b3901fa32ec131fd2650a060bd7da2950a49
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-20 18:59:23 +0300

    added part 1 task 5 iterative factorial

---
+++ Homework_2/task_5i.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int factorial(int number)
+{
+	if (number > 0)
+	{
+		int result = 1;
+		for (int i = 1; i <= number; i++)
+		{
+			result = result * i;
+		}
+		return result;
+	}
+	else if (number == 0)
+	{
+		return 1;
+	}
+	else if (number < 0)
+	{
+		return 0;
+	}
+}
+int main()
+{
+	int number = 0;
+	scanf_s("%d", &number);
+	printf("%d", factorial(number));
+	return 0;
+}
