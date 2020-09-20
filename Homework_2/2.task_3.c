12f3014b52582d13febce603d79e9780e0644c9b
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-20 14:39:00 +0300

    The program logic change.
    В прошлой версии программы массив сортировался  "быстрой сортировкой" и поряд символов менялся. При таком алгоритме порядок сохраняется

--- Homework_2/2.task_3.c
+++ Homework_2/2.task_3.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 const int N = 11;
-int sorter(const void* b, const void* a)
-{
-	return *(int*)a - *(int*)b;
-}
 int main()
 {
-	int arr[11] = { 5, 0, 1, 5, 2, 0, 6, 0, 3, 7, 0 };
+	int arr[11] = { 5,0,1,5,2,0,6,0,3,7,0 };
 	int i = 0;
-	qsort(arr, N, sizeof(int), sorter);
+	for (i = 0; i < N; i++)
+	{
+		if (arr[i] == 0)
+		{
+			for (int j = i; j < N-1; j++)
+			{
+				arr[j] = arr[j + 1];
+			}
+			arr[N - 1] = 0;
+		}
+	}
 	for (i = 0; i < N; ++i)
 	{
 		printf("%d ", arr[i]);
 	}
 }
+//В первом файле на HWproj используется другая логика программы, где массив сортируется
