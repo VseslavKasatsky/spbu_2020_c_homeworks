1924d0a8387d8daa2451867dad4be0911424d7b6
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-21 00:22:54 +0300

    Added Full Version v.1.0

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -1,20 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-int randomizer()
-{
-	int number = 1;
-	srand(1000);
-	number = number + rand();
-	return number % 10000;
-}
+#include <time.h>
+
 int main()
 {
-	int number = randomizer(), arr[10]={0},check;
-	printf("%d", number);
-	while(1)
+	int i, number_arr[4] = { 0 }, j, check_arr[4] = { 0 }, check, flag = 1;
+	for (i = 0; i < 4; i++)
 	{
-		scanf_s("%d",&check);
+		srand(time(NULL));
+		while (i < 4)
+		{
+			number_arr[i] = rand() % 10;
+			for (j = 0; j < i; j++)
+			{
+				if (number_arr[i] == number_arr[j])
+				{
+					flag = 0;
+					break;
+				}
+			}
+			if (flag)
+			{
+				printf("%d",number_arr[i]);
+				i++;
+			}
+			flag = 1;
+		}
 	}
+	while (1)
+	{
+		int cow = 0, bull = 0;
+		scanf_s("%d", &check);
+		for (i = 3; i >= 0; i--)
+		{
+			check_arr[i] = check % 10;
+			check = check / 10;
+		}
+		for (i = 0; i < 4; i++)
+		{
+			for (j = 0; j < 4; j++)
+			{
+				if (check_arr[i] == number_arr[j])
+				{
+					if (i == j)
+					{
+						bull++;
+					}
+					else
+					{
+						cow++;
+					}
+				}
+			}
+		}
+		if (bull != 4)
+		{
+			printf("%db %dc", bull, cow);
+		}
+		else
+		{
+			printf("YOU WIN!");
+			break;
+		}
+
+	}
+
 	return 0;
 }
 
