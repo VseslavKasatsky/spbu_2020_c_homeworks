39b4752645eebfd63f67480ce08465c91447c549
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 01:17:44 +0300

    try 1000 to fix style guide

--- Homework_1/task_1.c
+++ Homework_1/task_1.c
@@ -2,52 +2,42 @@
 
 int const SIZE = 7;
 
-int
-main()
+int main()
 {
-	int array[SIZE][SIZE];
-	int i = 0, j = 0;
-	for (i = 0; i < SIZE; i++)
-	{
-		for (j = 0; j < SIZE; j++)
-		{
-			array[i][j] = j + 1;
-		}
-	}
-	int coordinate_x = SIZE / 2, coordinate_y = SIZE / 2, circle = 0;
-	printf("%d", array[coordinate_y][coordinate_x]);
-	while (coordinate_x != 0 && coordinate_y != SIZE - 1)
-	{
-		if (circle < SIZE - 1)
-		{
-			circle++;
-		}
-		for (i = 0; i < circle; i++)
-		{
-			coordinate_y = coordinate_y + 1;
-			printf("%d", array[coordinate_y][coordinate_x]);
-		}
-		for (i = 0; i < circle; i++)
-		{
-			coordinate_x = coordinate_x + 1;
-			printf("%d", array[coordinate_y][coordinate_x]);
-		}
-		circle++;
-		for (i = 0; i < circle; i++)
-		{
-			coordinate_y = coordinate_y - 1;
-			printf("%d", array[coordinate_y][coordinate_x]);
-		}
-		for (i = 0; i < circle; i++)
-		{
-			coordinate_x = coordinate_x - 1;
-			printf("%d", array[coordinate_y][coordinate_x]);
-		}
-	}
-	for (i = 0; i < circle; i++)
-	{
-		coordinate_y = coordinate_y + 1;
-		printf("%d", array[coordinate_y][coordinate_x]);
-	}
-	return 0;
+    int array[SIZE][SIZE];
+    int i = 0, j = 0;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            array[i][j] = j + 1;
+        }
+    }
+    int coordinate_x = SIZE / 2, coordinate_y = SIZE / 2, circle = 0;
+    printf("%d", array[coordinate_y][coordinate_x]);
+    while (coordinate_x != 0 && coordinate_y != SIZE - 1) {
+        if (circle < SIZE - 1) {
+            circle++;
+        }
+        for (i = 0; i < circle; i++) {
+            coordinate_y = coordinate_y + 1;
+            printf("%d", array[coordinate_y][coordinate_x]);
+        }
+        for (i = 0; i < circle; i++) {
+            coordinate_x = coordinate_x + 1;
+            printf("%d", array[coordinate_y][coordinate_x]);
+        }
+        circle++;
+        for (i = 0; i < circle; i++) {
+            coordinate_y = coordinate_y - 1;
+            printf("%d", array[coordinate_y][coordinate_x]);
+        }
+        for (i = 0; i < circle; i++) {
+            coordinate_x = coordinate_x - 1;
+            printf("%d", array[coordinate_y][coordinate_x]);
+        }
+    }
+    for (i = 0; i < circle; i++) {
+        coordinate_y = coordinate_y + 1;
+        printf("%d", array[coordinate_y][coordinate_x]);
+    }
+    return 0;
 }
--- Homework_1/task_5.c
+++ Homework_1/task_5.c
@@ -3,29 +3,24 @@
 
 int const SIZE = 9;
 
-int
-helper(const void* a, const void* b)
+int helper(const void* a, const void* b)
 {
-	return *(int*)a - *(int*)b;
+    return *(int*)a - *(int*)b;
 }
 
-int
-main()
+int main()
 {
-	int i = 0, answer = 0, array[SIZE] = { 0 };
-	for (i = 0; i < SIZE; i++)
-	{
-		scanf("%d", &array[i]);
-	}
-	qsort(array, SIZE, sizeof(int), helper);
-	for (i = SIZE - 1; i > 0; i--)
-	{
-		if (array[i] == array[i - 1])
-		{
-			answer = array[i];
-			break;
-		}
-	}
-	printf("%d", answer);
-	return 0;
+    int i = 0, answer = 0, array[SIZE] = { 0 };
+    for (i = 0; i < SIZE; i++) {
+        scanf("%d", &array[i]);
+    }
+    qsort(array, SIZE, sizeof(int), helper);
+    for (i = SIZE - 1; i > 0; i--) {
+        if (array[i] == array[i - 1]) {
+            answer = array[i];
+            break;
+        }
+    }
+    printf("%d", answer);
+    return 0;
 }
