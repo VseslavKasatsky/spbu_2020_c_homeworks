9cb5bf0a25b5fd480942f3b6d2d8691fc41e8c23
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 00:48:51 +0300

    update style guide

--- Homework_1/task_4.c
+++ Homework_1/task_4.c
@@ -1,20 +1,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-int main() {
-  int number = 0;
-  scanf("%d", &number);
-  for (int i = 2; i < number; i++) {
-    bool flag = 1;
-    for (int j = 2; j < i; j++) {
-      if (i % j == 0) {
-        flag = 0;
-        break;
-      }
+int main()
+{
+    int number = 0;
+    scanf("%d", &number);
+    for (int i = 2; i < number; i++) {
+        bool flag = 1;
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag) {
+            printf("%d ", i);
+        }
     }
-    if (flag) {
-      printf("%d ", i);
-    }
-  }
-  return 0;
+    return 0;
 }
--- Homework_1/task_5.c
+++ Homework_1/task_5.c
@@ -3,20 +3,21 @@
 
 int const SIZE = 9;
 
-int helper(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+int helper(const void* a, const void* b) { return *(int*)a - *(int*)b; }
 
-int main() {
-  int i = 0, answer = 0, array[SIZE] = {0};
-  for (i = 0; i < SIZE; i++) {
-    scanf("%d", &array[i]);
-  }
-  qsort(array, SIZE, sizeof(int), helper);
-  for (i = SIZE - 1; i > 0; i--) {
-    if (array[i] == array[i - 1]) {
-      answer = array[i];
-      break;
+int main()
+{
+    int i = 0, answer = 0, array[SIZE] = { 0 };
+    for (i = 0; i < SIZE; i++) {
+        scanf("%d", &array[i]);
     }
-  }
-  printf("%d", answer);
-  return 0;
+    qsort(array, SIZE, sizeof(int), helper);
+    for (i = SIZE - 1; i > 0; i--) {
+        if (array[i] == array[i - 1]) {
+            answer = array[i];
+            break;
+        }
+    }
+    printf("%d", answer);
+    return 0;
 }
