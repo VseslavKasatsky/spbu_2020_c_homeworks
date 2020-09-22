a6b5dc3ea58272b0e7fb58126c6b6e49bc750a5d
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 01:54:18 +0300

    Fix Buggs

--- Homework_2/2.task_3.c
+++ Homework_2/2.task_3.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 const int N = 11;
-int main()
-{
-	int arr[11] = { 5, 0, 1, 5, 2, 0, 6, 0, 3, 7, 0 };
-	int i = 0;
-	for (i = 0; i < N; i++)
-	{
-		if (arr[i] == 0)
-		{
-			for (int j = i; j < N - 1; j++)
-			{
-				arr[j] = arr[j + 1];
-			}
-			arr[N - 1] = 0;
-		}
-	}
-	for (i = 0; i < N; ++i)
-	{
-		printf("%d ", arr[i]);
-	}
+int main() {
+  int array[N], counter_start = 0, counter_end = N - 1, vault = 0;
+  for (int i = 0; i < N; ++i) {
+    scanf("%d", &array[i]);
+  }
+  for (; counter_start < counter_end; ++counter_start) {
+    while (array[counter_end] == 0) {
+      counter_end--;
+    }
+    if (array[counter_start] == 0) {
+      vault = array[counter_start];
+      array[counter_start] = array[counter_end];
+      array[counter_end] = vault;
+      counter_end--;
+    }
+  }
+  for (int i = 0; i < N; ++i) {
+    printf("%d ", array[i]);
+  }
 }
