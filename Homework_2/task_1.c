7bbe45a89495c7ae0294684b096d6b878eb18b13
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 02:50:14 +0300

    UPDATE
    1. Remove global variables
    2. FIXED style guide
    3. added user's description

--- Homework_2/task_1.c
+++ Homework_2/task_1.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int size = 0;
-int* array = 0;
-int* values = NULL;
-void print()
+void print_answer_to_console(int size, int* values)
 {
-	printf("%d", values[0]);
-	for (int i = 1; i < size; i++)
-		printf("+%d", values[i]);
-	printf("\n");
+    printf("%d", values[0]);
+    for (int i = 1; i < size; ++i)
+        printf("+%d", values[i]);
+    printf("\n");
 }
 
-void recursion(int left_sum, int last_term)
+void decomposition_of_number(int left_sum, int last_term, int size, int* values)
 {
-	if (left_sum == 0)
-		print();
-	for (int i = last_term; i <= left_sum; i++)
-	{
-		size++;
-		array = (int*)realloc(values, sizeof(int) * (size));
-		values = array;
-		values[size - 1] = i;
-		recursion(left_sum - i, i);
-		size--;
-	}
+    if (left_sum == 0) {
+        print_answer_to_console(size, values);
+    }
+    for (int i = last_term; i <= left_sum; ++i) {
+        size++;
+        values = (int*)realloc(values, sizeof(int) * (size));
+        values[size - 1] = i;
+        decomposition_of_number(left_sum - i, i, size, values);
+        size--;
+    }
 }
 int main()
 {
-	int number = 0;
-	scanf("%d", &number);
-	recursion(number, 1);
-	free(values);
-	return 0;
+    int number = 0, size = 0, *values = NULL;
+    printf("Enter the number to decompose : ");
+    scanf("%d", &number);
+    decomposition_of_number(number, 1, size, values);
+    free(values);
+    return 0;
 }
