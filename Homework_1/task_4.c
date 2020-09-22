2c2ef85dadf1e6222c696fbacff241d39c22c28b
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-22 23:19:39 +0300

    style guide upd

--- Homework_1/task_4.c
+++ Homework_1/task_4.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main()
-{
-	int number = 0;
-	scanf("%d", &number);
-	for (int i = 2; i < number; i++)
-	{
-		bool flag = 1;
-		for (int j = 2; j < i; j++)
-		{
-			if (i % j == 0)
-			{
-				flag = 0;
-				break;
-			}
-		}
-		if (flag)
-		{
-			printf("%d ", i);
-		}
+int main() {
+  int number = 0;
+  scanf("%d", &number);
+  for (int i = 2; i < number; i++) {
+	bool flag = 1;
+	for (int j = 2; j < i; j++) {
+	  if (i % j == 0) {
+		flag = 0;
+		break;
+	  }
 	}
-	return 0;
+	if (flag) {
+	  printf("%d ", i);
+	}
+  }
+  return 0;
 }
