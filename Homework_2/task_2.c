15b853ac5c6114d6004985318d3dafefccd081fc
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 02:18:24 +0300

    style guide fix

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -2,51 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-  int i = 0, number_arr[4] = {0}, j = 0, check_arr[4], check = 0, flag = 1;
-  for (; i < 4; ++i) {
-    srand(time(NULL));
-    while (i < 4) {
-      number_arr[i] = rand() % 10;
-      for (j = 0; j < i; ++j) {
-        if (number_arr[i] == number_arr[j]) {
-          flag = 0;
-          break;
+int main()
+{
+    int i = 0, number_arr[4] = { 0 }, j = 0, check_arr[4], check = 0, flag = 1;
+    for (; i < 4; ++i) {
+        srand(time(NULL));
+        while (i < 4) {
+            number_arr[i] = rand() % 10;
+            for (j = 0; j < i; ++j) {
+                if (number_arr[i] == number_arr[j]) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag) {
+                // printf("%d", number_arr[i]); - answer
+                i++;
+            }
+            flag = 1;
         }
-      }
-      if (flag) {
-        // printf("%d", number_arr[i]); - answer
-        i++;
-      }
-      flag = 1;
     }
-  }
-  while (1) {
-    int cow = 0, bull = 0;
-    printf("OTBET: ");
-    scanf("%d", &check);
-    for (i = 3; i >= 0; --i) {
-      check_arr[i] = check % 10;
-      check = check / 10;
-    }
-    for (i = 0; i < 4; ++i) {
-      for (j = 0; j < 4; ++j) {
-        if (check_arr[i] == number_arr[j]) {
-          if (i == j) {
-            bull++;
-          } else {
-            cow++;
-          }
+    while (1) {
+        int cow = 0, bull = 0;
+        printf("OTBET: ");
+        scanf("%d", &check);
+        for (i = 3; i >= 0; --i) {
+            check_arr[i] = check % 10;
+            check = check / 10;
         }
-      }
+        for (i = 0; i < 4; ++i) {
+            for (j = 0; j < 4; ++j) {
+                if (check_arr[i] == number_arr[j]) {
+                    if (i == j) {
+                        bull++;
+                    } else {
+                        cow++;
+                    }
+                }
+            }
+        }
+        if (bull != 4) {
+            printf("%d bull %d cow \n", bull, cow);
+        } else {
+            printf("YOU WIN!");
+            break;
+        }
     }
-    if (bull != 4) {
-      printf("%d bull %d cow \n", bull, cow);
-    } else {
-      printf("YOU WIN!");
-      break;
-    }
-  }
 
-  return 0;
+    return 0;
 }
