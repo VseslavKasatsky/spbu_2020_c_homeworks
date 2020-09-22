0426541a0d32c27240b225031fa02895121a72df
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 01:59:27 +0300

    update style guide

--- Homework_2/2.task_3.c
+++ Homework_2/2.task_3.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 const int N = 11;
-int main() {
-  int array[N], counter_start = 0, counter_end = N - 1, vault = 0;
-  for (int i = 0; i < N; ++i) {
-    scanf("%d", &array[i]);
-  }
-  for (; counter_start < counter_end; ++counter_start) {
-    while (array[counter_end] == 0) {
-      counter_end--;
+
+    int main() {
+      int array[N], counter_start = 0, counter_end = N - 1, vault = 0;
+      for (int i = 0; i < N; ++i) {
+        scanf("%d", &array[i]);
+      }
+      for (; counter_start < counter_end; ++counter_start) {
+        while (array[counter_end] == 0) {
+          counter_end--;
+        }
+        if (array[counter_start] == 0) {
+          vault = array[counter_start];
+          array[counter_start] = array[counter_end];
+          array[counter_end] = vault;
+          counter_end--;
+        }
+      }
+      for (int i = 0; i < N; ++i) {
+        printf("%d ", array[i]);
+      }
     }
-    if (array[counter_start] == 0) {
-      vault = array[counter_start];
-      array[counter_start] = array[counter_end];
-      array[counter_end] = vault;
-      counter_end--;
-    }
-  }
-  for (int i = 0; i < N; ++i) {
-    printf("%d ", array[i]);
-  }
-}
