a7cbc35798760cb68aad42e8037537d043a27b38
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-22 23:23:58 +0300

    style fix

--- Homework_1/task_4.c
+++ Homework_1/task_4.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-  int number = 0;
-  scanf("%d", &number);
-  for (int i = 2; i < number; i++) {
-	bool flag = 1;
-	for (int j = 2; j < i; j++) {
-	  if (i % j == 0) {
-		flag = 0;
-		break;
-	  }
+int
+main()
+{
+	int number = 0;
+	scanf("%d", &number);
+	for (int i = 2; i < number; i++)
+	{
+		bool flag = 1;
+		for (int j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+		{
+			printf("%d ", i);
+		}
 	}
-	if (flag) {
-	  printf("%d ", i);
-	}
-  }
-  return 0;
+	return 0;
 }
