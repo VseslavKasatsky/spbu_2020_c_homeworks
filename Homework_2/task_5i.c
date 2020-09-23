ddbf83bdd859caf6c51918fcbe6b327970af5d4d
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 12:51:34 +0300

    HotFix with ruturn and if

--- Homework_2/task_5i.c
+++ Homework_2/task_5i.c
@@ -4,13 +4,11 @@ int factorial(int number)
     if (number < 0) {
         return 0;
     }
-    if (number >= 0) {
-        int result = 1;
-        for (int i = 1; i <= number; ++i) {
-            result = result * i;
-        }
-        return result;
+    int result = 1;
+    for (int i = 1; i <= number; ++i) {
+        result = result * i;
     }
+    return result;
 }
 int main()
 {
--- Homework_2/task_5r.c
+++ Homework_2/task_5r.c
@@ -8,9 +8,7 @@ int factorial(int number)
     if (number == 0) {
         return 1;
     }
-    if (number >= 1) {
-        return number * factorial(number - 1);
-    }
+    return number * factorial(number - 1);
 }
 int main()
 {
