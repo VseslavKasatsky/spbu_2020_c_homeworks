cfe3a6fbfc68c0f64b6f7df4950f9585005e84dc
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 19:19:12 +0300

    Update initialization

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -1,15 +1,17 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main()
 {
-    int i = 0, number_arr[4] = { 0 }, j = 0, check_arr[4], check = 0, flag = 1;
-    for (; i < 4; ++i) {
+    int number_arr[4] = { 0 }, check_arr[4], check = 0;
+    bool flag = 1;
+    for (int i = 0; i < 4; ++i) {
         srand(time(NULL));
         while (i < 4) {
             number_arr[i] = rand() % 10;
-            for (j = 0; j < i; ++j) {
+            for (int j = 0; j < i; ++j) {
                 if (number_arr[i] == number_arr[j]) {
                     flag = 0;
                     break;
@@ -22,16 +24,17 @@ int main()
             flag = 1;
         }
     }
-    while (1) {
+
+    while (flag) {
         int cow = 0, bull = 0;
         printf("YOUR GUESS: ");
         scanf("%d", &check);
-        for (i = 3; i >= 0; --i) {
+        for (int i = 3; i >= 0; --i) {
             check_arr[i] = check % 10;
             check = check / 10;
         }
-        for (i = 0; i < 4; ++i) {
-            for (j = 0; j < 4; ++j) {
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
                 if (check_arr[i] == number_arr[j]) {
                     if (i == j) {
                         bull++;
@@ -44,10 +47,9 @@ int main()
         if (bull != 4) {
             printf("%d bull %d cow \n", bull, cow);
         } else {
-            printf("YOU WIN!");
-            break;
+            flag = 0;
         }
     }
-
+    printf("YOU WIN!");
     return 0;
 }
