e16925408625d466c32f39269db7aff22e5bb210
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 12:30:27 +0300

    Update
    Change int to long long int

--- Homework_2/2.task_4.c
+++ Homework_2/2.task_4.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
+int const DIGIT_AMOUNT = 10;
 int main()
 {
-	int number = 0, arr[10] = { 0 }, j;
-	scanf_s("%d", &number);
-	while (number > 0)
-	{
-		arr[number % 10]++;
-		number = number / 10;
-	}
-	number = 0;
-	if (arr[0] != 0)
-	{
-		for (j = 1; j < 10; j++)
-		{
-			if (arr[j] != 0)
-			{
-				number = j;
-				arr[j]--;
-				break;
-			}
-		}
-	}
-	for (j = 0; j < 10; j++)
-	{
-		while (arr[j] != 0)
-		{
-			number = number * 10 + j;
-			arr[j]--;
-		}
-	}
-	printf("%d", number);
+    long long int number = 0;
+    int array[DIGIT_AMOUNT];
+    for (int i = 0; i < DIGIT_AMOUNT; ++i) {
+        array[i] = 0;
+    }
+    scanf("%I64d", &number);
+    while (number > 0) {
+        array[number % 10]++;
+        number = number / 10;
+    }
+    if (array[0] != 0) {
+        int i = 0;
+        while (number == 0) {
+            if (array[i] != 0) {
+                number = i;
+                array[i]--;
+                i++;
+            }
+        }
+    }
+    for (int i = 0; i < DIGIT_AMOUNT; i++) {
+        while (array[i] != 0) {
+            number = number * 10 + i;
+            array[i]--;
+        }
+    }
+    printf("%I64d", number);
 }
