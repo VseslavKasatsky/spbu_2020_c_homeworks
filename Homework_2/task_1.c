9f43781c80bfacb633f05a44d1fd0e5e55c6eb4c
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 19:02:55 +0300

    nothing

--- Homework_2/task_1.c
+++ Homework_2/task_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void print_answer_to_console(int size, int* values)
+#include <crtdbg.h>
+void print_decomposed_number (int size, int* values)
 {
     printf("%d", values[0]);
     for (int i = 1; i < size; ++i)
@@ -9,16 +9,16 @@ void print_answer_to_console(int size, int* values)
     printf("\n");
 }
 
-void decomposition_of_number(int left_sum, int last_term, int size, int* values)
+void decompose_number(int left_sum, int last_term, int size, int* values)
 {
     if (left_sum == 0) {
-        print_answer_to_console(size, values);
+        print_decomposed_number (size, values);
     }
     for (int i = last_term; i <= left_sum; ++i) {
         size++;
         values = (int*)realloc(values, sizeof(int) * (size));
         values[size - 1] = i;
-        decomposition_of_number(left_sum - i, i, size, values);
+        decompose_number(left_sum - i, i, size, values);
         size--;
     }
 }
@@ -27,7 +27,7 @@ int main()
     int number = 0, size = 0, *values = NULL;
     printf("Enter the number to decompose : ");
     scanf("%d", &number);
-    decomposition_of_number(number, 1, size, values);
+    decompose_number(number, 1, size, values);
     free(values);
     return 0;
 }
