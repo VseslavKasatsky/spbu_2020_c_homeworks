bcf56d19ca19acadd55a30af6c2d7a5bcd8080ca
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 19:36:20 +0300

    Delete vault and fixed array=0,0,0...

--- Homework_2/2.task_3.c
+++ Homework_2/2.task_3.c
@@ -3,19 +3,18 @@ const int N = 11;
 
 int main()
 {
-    int array[N], vault = 0;
+    int array[11];
     for (int i = 0; i < N; ++i) {
         printf("array[%d]=", i);
         scanf("%d", &array[i]);
     }
     for (int counter_start = 0, counter_end = N - 1; counter_start < counter_end; ++counter_start) {
-        while (array[counter_end] == 0) {
+        while (array[counter_end] == 0 && counter_end > 0) {
             counter_end--; //finding a nonzero element at the end
         }
         if (array[counter_start] == 0) { //swap
-            vault = array[counter_start];
             array[counter_start] = array[counter_end];
-            array[counter_end] = vault;
+            array[counter_end] = 0;
             counter_end--;
         }
     }
