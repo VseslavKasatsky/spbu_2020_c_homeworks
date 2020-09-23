37f28b32469a77a961942f99db0cd51e68b408e3
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-23 12:45:27 +0300

    added comments

--- Homework_2/2.task_3.c
+++ Homework_2/2.task_3.c
@@ -3,16 +3,16 @@ const int N = 11;
 
 int main()
 {
-    int array[N], counter_start = 0, counter_end = N - 1, vault = 0;
+    int array[N], vault = 0;
     for (int i = 0; i < N; ++i) {
         printf("array[%d]=", i);
         scanf("%d", &array[i]);
     }
-    for (; counter_start < counter_end; ++counter_start) {
+    for (int counter_start = 0, counter_end = N - 1; counter_start < counter_end; ++counter_start) {
         while (array[counter_end] == 0) {
-            counter_end--;
+            counter_end--; //finding a nonzero element at the end
         }
-        if (array[counter_start] == 0) {
+        if (array[counter_start] == 0) { //swap
             vault = array[counter_start];
             array[counter_start] = array[counter_end];
             array[counter_end] = vault;
