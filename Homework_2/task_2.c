e12cad8718a0d2ae8cf2bed5aa9d2a7b8ce6e4ec
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-24 00:52:06 +0300

    UPDATE before review

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -2,40 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main()
+int const DIGITAL_RANGE = 4; //The length of the guessed sequential
+void generating_random_number(int* random_number_array)
 {
-    int number_arr[4] = { 0 }, check_arr[4], check = 0;
     bool flag = 1;
     for (int i = 0; i < 4; ++i) {
         srand(time(NULL));
         while (i < 4) {
-            number_arr[i] = rand() % 10;
+            random_number_array[i] = rand() % 10;
             for (int j = 0; j < i; ++j) {
-                if (number_arr[i] == number_arr[j]) {
+                if (random_number_array[i] == random_number_array[j]) {
                     flag = 0;
                     break;
                 }
             }
             if (flag) {
-                // printf("%d", number_arr[i]); - answer
+                printf("%d", random_number_array[i]);
                 i++;
             }
             flag = 1;
         }
     }
-
+}
+void start_game(int* random_number_array)
+{
+    bool flag = 1;
+    int user_number_array[4] = { 0 }, user_number = 0;
     while (flag) {
         int cow = 0, bull = 0;
         printf("YOUR GUESS: ");
-        scanf("%d", &check);
+        scanf("%d", &user_number);
         for (int i = 3; i >= 0; --i) {
-            check_arr[i] = check % 10;
-            check /= 10;
+            user_number_array[i] = user_number % 10;
+            user_number /= 10; //convert user number to array
         }
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
-                if (check_arr[i] == number_arr[j]) {
+                if (random_number_array[i] == user_number_array[j]) {
                     if (i == j) {
                         bull++;
                     } else {
@@ -51,5 +54,12 @@ int main()
         }
     }
     printf("YOU WIN!");
+}
+int main()
+{
+    int random_number_array[4];
+    int* link_to_random_array = random_number_array;
+    generating_random_number(link_to_random_array);
+    start_game(link_to_random_array);
     return 0;
 }
