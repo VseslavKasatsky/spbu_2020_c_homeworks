6221044ade583866a95210c4b2dcaf86fc5c17f7
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-24 17:19:21 +0300

    Update constant and link

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -6,9 +6,9 @@ int const DIGITAL_RANGE = 4; //The length of the guessed sequential
 void generating_random_number(int* random_number_array)
 {
     bool flag = 1;
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < DIGITAL_RANGE; ++i) {
         srand(time(NULL));
-        while (i < 4) {
+        while (i < DIGITAL_RANGE) {
             random_number_array[i] = rand() % 10;
             for (int j = 0; j < i; ++j) {
                 if (random_number_array[i] == random_number_array[j]) {
@@ -27,17 +27,17 @@ void generating_random_number(int* random_number_array)
 void start_game(int* random_number_array)
 {
     bool flag = 1;
-    int user_number_array[4] = { 0 }, user_number = 0;
+    int user_number_array[DIGITAL_RANGE] = { 0 }, user_number = 0;
     while (flag) {
         int cow = 0, bull = 0;
         printf("YOUR GUESS: ");
         scanf("%d", &user_number);
-        for (int i = 3; i >= 0; --i) {
+        for (int i = DIGITAL_RANGE - 1; i >= 0; --i) {
             user_number_array[i] = user_number % 10;
             user_number /= 10; //convert user number to array
         }
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
+        for (int i = 0; i < DIGITAL_RANGE; ++i) {
+            for (int j = 0; j < DIGITAL_RANGE; ++j) {
                 if (random_number_array[i] == user_number_array[j]) {
                     if (i == j) {
                         bull++;
@@ -47,7 +47,7 @@ void start_game(int* random_number_array)
                 }
             }
         }
-        if (bull != 4) {
+        if (bull != DIGITAL_RANGE) {
             printf("%d bull %d cow \n", bull, cow);
         } else {
             flag = 0;
@@ -57,9 +57,8 @@ void start_game(int* random_number_array)
 }
 int main()
 {
-    int random_number_array[4];
-    int* link_to_random_array = random_number_array;
-    generating_random_number(link_to_random_array);
-    start_game(link_to_random_array);
+    int random_number_array[DIGITAL_RANGE];
+    generating_random_number((int*)&random_number_array);
+    start_game((int*)&random_number_array);
     return 0;
 }
