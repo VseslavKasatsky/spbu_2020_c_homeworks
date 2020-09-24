a4af97cedd1b60c2de81f86589311d8b1ae93e6f
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-24 20:43:32 +0300

    Added new type of memory, rename flag and others

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -2,63 +2,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int const DIGITAL_RANGE = 4; //The length of the guessed sequential
-void generating_random_number(int* random_number_array)
+
+void fill_with_random_numbers(int* random_number, int DIGITAL_RANGE)
 {
-    bool flag = 1;
+    bool isEqual = true;
     for (int i = 0; i < DIGITAL_RANGE; ++i) {
         srand(time(NULL));
         while (i < DIGITAL_RANGE) {
-            random_number_array[i] = rand() % 10;
+            random_number[i] = rand() % 10;
             for (int j = 0; j < i; ++j) {
-                if (random_number_array[i] == random_number_array[j]) {
-                    flag = 0;
+                if (random_number[i] == random_number[j]) {
+                    isEqual = false;
                     break;
                 }
             }
-            if (flag) {
-                printf("%d", random_number_array[i]);
+            if (isEqual) {
+                printf("%d", random_number[i]);
                 i++;
             }
-            flag = 1;
+            isEqual = true;
         }
     }
 }
-void start_game(int* random_number_array)
+void get_user_guesses(int* user_number, int DIGITAL_RANGE)
 {
-    bool flag = 1;
-    int user_number_array[DIGITAL_RANGE], user_number = 0;
-    while (flag) {
-        int cow = 0, bull = 0;
-        printf("YOUR GUESS: ");
-        scanf("%d", &user_number);
-        for (int i = DIGITAL_RANGE - 1; i >= 0; --i) {
-            user_number_array[i] = user_number % 10;
-            user_number /= 10; //convert user number to array
-        }
-        for (int i = 0; i < DIGITAL_RANGE; ++i) {
-            for (int j = 0; j < DIGITAL_RANGE; ++j) {
-                if (random_number_array[i] == user_number_array[j]) {
-                    if (i == j) {
-                        bull++;
-                    } else {
-                        cow++;
-                    }
+    int entered_number = 0;
+    printf("YOUR GUESS: ");
+    scanf("%d", &entered_number);
+    for (int i = DIGITAL_RANGE - 1; i >= 0; --i) {
+        user_number[i] = entered_number % 10;
+        entered_number /= 10; //convert user number to array
+    }
+}
+void count_equal_numbers(int* user_number, int* random_number, int& cow, int& bull, int DIGITAL_RANGE)
+{
+    for (int i = 0; i < DIGITAL_RANGE; ++i) {
+        for (int j = 0; j < DIGITAL_RANGE; ++j) {
+            if (random_number[i] == user_number[j]) {
+                if (i == j) {
+                    bull++;
+                } else {
+                    cow++;
                 }
             }
         }
+    }
+}
+void start_game(int* random_number, int DIGITAL_RANGE)
+{
+    bool isGuessed = true;
+    int* user_number = (int*)malloc(DIGITAL_RANGE * sizeof(int));
+    ;
+    while (isGuessed) {
+        int cow = 0, bull = 0;
+        get_user_guesses(user_number, DIGITAL_RANGE);
+        count_equal_numbers(user_number, random_number, (int&)cow, (int&)bull, DIGITAL_RANGE);
         if (bull != DIGITAL_RANGE) {
             printf("%d bull %d cow \n", bull, cow);
         } else {
-            flag = 0;
+            isGuessed = false;
         }
     }
     printf("YOU WIN!");
+    free(user_number);
 }
 int main()
 {
-    int random_number_array[DIGITAL_RANGE];
-    generating_random_number((int*)&random_number_array);
-    start_game((int*)&random_number_array);
+    int DIGITAL_RANGE = 0;
+    printf("Enter the length of the number to solve: ");
+    scanf("%d", &DIGITAL_RANGE);
+    int* random_number = (int*)malloc(DIGITAL_RANGE * sizeof(int));
+    fill_with_random_numbers(random_number, DIGITAL_RANGE);
+    start_game(random_number, DIGITAL_RANGE);
+    free(random_number);
     return 0;
 }
