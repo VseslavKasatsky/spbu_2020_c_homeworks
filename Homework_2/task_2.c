63cc9c811c07a4d2757f3db85627c8351b8c4dc6
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-09-27 11:57:57 +0300

    style test

--- Homework_2/task_2.c
+++ Homework_2/task_2.c
@@ -14,7 +14,7 @@ int get_unique_number(bool* digital_array)
 }
 void fill_with_random_numbers(int* random_numbers, int digital_range)
 {
-    bool digital_array[10] = { false };
+    bool digital_array[DIGIT_RANGE] = { false };
     for (int i = 0; i < digital_range; ++i) {
         random_numbers[i] = get_unique_number((bool*)&digital_array);
         digital_array[random_numbers[i]] = true;
