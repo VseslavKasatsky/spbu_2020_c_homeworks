329fbffba8bdcc47ae4e3371d62c2c0e81572bc3
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-10-13 12:55:28 +0300

    styleGuide

--- Test/task_2.c
+++ Test/task_2.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
 #include "../library/commonUtils/test1_2.h"
+#include <stdio.h>
 int main()
 {
-    int imaginaryFirst=0;
+    int imaginaryFirst = 0;
     printf("Enter first imaginary number");
-    scanf("%d",&imaginaryFirst);
-    int realNumberFirst=0;
+    scanf("%d", &imaginaryFirst);
+    int realNumberFirst = 0;
     printf("Enter first real number");
-    scanf("%d",&realNumberFirst);
+    scanf("%d", &realNumberFirst);
     //Не успел дописать демонстрацию функционала
-    int imaginarySecond=0;
+    int imaginarySecond = 0;
     printf("Enter second imaginary number");
-    scanf("%d",&imaginarySecond);
-    int realNumberSecond=0;
+    scanf("%d", &imaginarySecond);
+    int realNumberSecond = 0;
     printf("Enter second real number");
-    scanf("%d",&realNumberSecond);
+    scanf("%d", &realNumberSecond);
     return 0;
 }
