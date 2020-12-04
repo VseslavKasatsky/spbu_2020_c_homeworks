b0e62bfe2ebeaf500f367f66bf72a895057caad5
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-12-04 22:00:40 +0300

    style

--- library/commonUtils/list.h
+++ library/commonUtils/list.h
@@ -10,7 +10,7 @@ ListElement* createListElement(int value);
 ListElement* tail(List* list);
 ListElement* head(List* list);
 ListElement* retrieve(int position, List* list);
-ListElement* getNextInCycle(List* list, ListElement*previousElement);
+ListElement* getNextInCycle(List* list, ListElement* previousElement);
 
 bool insert(ListElement* element, int position, List* list);
 bool deleteElementOnPosition(int position, List* list);
@@ -23,6 +23,5 @@ void printList(List* list);
 int getSize(List* list);
 int locate(ListElement* element, List* list);
 int getElementValue(ListElement* element);
-
 
 #endif // SPBU_2020_C_HOMEWORKS_LIBRARY_commonUtils_LIST_H_
