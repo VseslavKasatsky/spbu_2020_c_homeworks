f446d38d89527e7f786ca0db17b180f65ff2a581
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2020-12-05 17:24:49 +0300

    added stringOperations library,

--- Homework_8/task_1.c
+++ Homework_8/task_1.c
@@ -1,28 +1,8 @@
 #include "../library/commonUtils/dfa.h"
+#include "../library/commonUtils/stringOperations.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-char* getString()
-{
-    int size = 1;
-    char* word = (char*)malloc(size * sizeof(char));
-    char buffer = '0';
-    printf("Enter the expression: ");
-    scanf("%c", &buffer);
-    int index = 0;
-    while (buffer != '\n' && buffer != '\0') {
-        word[index] = buffer;
-        scanf("%c", &buffer);
-        ++index;
-        if (size == index) {
-            size = size * 2;
-            word = (char*)realloc(word, size * sizeof(char));
-        }
-    }
-    word[index] = '\0';
-    return word;
-}
 
 void initializeStatesAndPushTransitions(DFAState** states)
 {
